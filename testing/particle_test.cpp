ea7881efbf0007ba490ac8803591b15b3f53bb50
micahc <micahc@b3ba8586-d01d-0410-af51-a98ec15c9dce>
2009-02-28 19:27:46 +0000

    Woot, sort of done (but not really).
    And by done I mean theoretically I have the particle filter worked out.

--- testing/particle_test.cpp
+++ testing/particle_test.cpp
@@ -5,15 +5,19 @@
 #include <indii/ml/filter/ParticleFilter.hpp>
 #include <indii/ml/filter/ParticleFilterModel.hpp>
 #include <indii/ml/filter/StratifiedParticleResampler.hpp>
-#include <indii/ml/aux/DiracMixturePdf.hpp>
+#include <indii/ml/aux/GaussianPdf.hpp>
 #include <indii/ml/aux/vector.hpp>
 #include <indii/ml/aux/matrix.hpp>
 
 #define SYSTEM_SIZE 2
 #define MEAS_SIZE 1
+#define INPUT_SIZE 1
 #define ACTUAL_SIZE 3
 #define STEPS 250
 #define NUM_PARTICLES 1000
+
+#define A1 3.4
+#define A2 1.0
 
 using namespace std;
 using namespace indii::ml::filter;
@@ -38,25 +42,43 @@ public:
   BoldModel();
   
   unsigned int getStateSize() { return 4; };
+  unsigned int getStimSize() { return 1; };
   unsigned int getMeasurementSize() { return MEAS_SIZE; };
 
   aux::vector transition(const aux::vector& s,
       const double t, const double delta);
+  aux::vector transition(const aux::vector& s,
+      const double t, const double delta, const aux::vector& u);
 
   aux::vector measure(const aux::vector& s);
 
-  double weight(const aux::vector& s,
-      const aux::vector& y);
+  double weight(const aux::vector& s, const aux::vector& y);
 
 private:
   double V_0;
   double a_1;
   double a_2;
+  double tau_0;
+  double tau_s;
+  double tau_f;
+  double alpha;
+  double E_0;
+  double epsilon;
+  double var_e;
+  double small_g;
 };
 
 BoldModel::BoldModel()
 {
-    
+  tau_s = 4.98;
+  tau_f = 8.31;
+  epsilon = 0.069;
+  tau_0 = 8.38;
+  alpha = .189;
+  E_0 = .635;
+  V_0 = 1.49e-2;
+  small_g = .95e-5;
+  var_e = 3.92e-6;
 }
 
 BoldModel::~BoldModel()
@@ -67,42 +89,55 @@ BoldModel::~BoldModel()
 aux::vector BoldModel::transition(const aux::vector& s,
         const double t, const double delta)
 {
+    aux::zero_vector u(INPUT_SIZE);
+    return transition(s, t, delta, u);
+}
+
+aux::vector BoldModel::transition(const aux::vector& s,
+        const double t, const double delta, const aux::vector& u)
+{
+    //TODO, potentially add some randomness here.
+    
     aux::vector w(SYSTEM_SIZE);
     //V_t* = (1/tau_0) * ( f_t - v_t ^ (1/\alpha)) 
-    s(0) = (1./params->named.tau_0) * (state->named.f_t -
-                pow(state->named.v_t, 1./params->named.alpha));
+    double dot = (1./tau_0) * (s(3) - pow(s(0), 1./alpha));
+    w(0) = s(0) + dot*delta;
     
     //Q_t* = ...
-    double A = (state->named.f_t / params->named.e_0) * 
-                (1 - pow( 1. - params->named.e_0, 1./state->named.f_t));
-    double B = state->named.q_t / pow(state->named.v_t, 1.-1./params->named.alpha);
-    tmp.named.q_t = (1./params->named.tau_0) * ( A - B );
+    double A = (s(3) / E_0) * (1 - pow( 1. - E_0, 1./s(3)));
+    double B = s(1) / pow(s(0), 1.-1./alpha);
+    dot = (1./tau_0) * ( A - B );
+    w(1) = s(1) + dot*delta;
 
     //S_t* = \epsilon*u_t - 1/\tau_s * s_t - 1/\tau_f * (f_t - 1)
-    tmp.named.s_t = params->named.epsilon*extradata->control - 
-                state->named.s_t/params->named.tau_s - 
-                (state->named.f_t - 1.) / params->named.tau_f;
+    dot = u(0)*epsilon - s(2)/tau_s - (s(3) - 1.) / tau_f;
+    w(2) = s(2) + dot*delta;
 
-    tmp.named.f_t = state->named.s_t;
-
-    //X_t = X_{t-1} + dx*dt
-    for(int i = 0 ; i < 4 ; i++) {
-        dist[i][0] = state->array[i] + tmp.array[i]*extradata->delta_t;
-        dist[i][1] = extradata->gg*extradata->delta_t;
-    }
-    
+    //f_t* = s_t;
+    dot = s(2);
+    w(3) = s(3) + dot*delta;
+    return w;
 }
 
 aux::vector BoldModel::measure(const aux::vector& s)
 {
     aux::vector y(MEAS_SIZE);
-    y(0) = V_0 * ( a_1 * ( 1 - s(1)) - a_2 * (1 - s(0)));
+    y(0) = V_0 * ( A1 * ( 1 - s(1)) - A2 * (1 - s(0)));
     return y;
 }
 
 double BoldModel::weight(const aux::vector& s, const aux::vector& y)
 {
-
+    //these are really constant throughout the execution
+    //of the program, so no need to calculate over and over
+    static aux::symmetric_matrix cov(1);
+    cov(0,0) = var_e;
+    static aux::GaussianPdf rng(aux::zero_vector(1), cov);
+    
+    aux::vector location(1);
+    location(0) = y(0) - measure(s)(0);
+    
+    return rng.densityAt(location);
 }
 
 int main(int argc, char* argv[])
