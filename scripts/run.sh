25b2dce3d6f26875ae49e4798a8dfe2773ceaadb
micahc <micahc@b3ba8586-d01d-0410-af51-a98ec15c9dce>
2009-04-13 17:56:50 +0000

    Added some basic stuff to run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,11 @@
-./brainid simseries.nii.gz stim.in distribution_2.serial distribution.serial
+#!/bin/bash
+mkdir "run$1"
+cd "run$1"
+if [[ $1 != 1 ]]
+then
+    ../brainid ../simseries.nii.gz ../stim.in distribution.serial ../distribution.serial
+else
+    ../brainid ../simseries.nii.gz ../stim.in distribution.serial
+fi
+ln -s distribution.serial ../distribution.serial
+cd ../
