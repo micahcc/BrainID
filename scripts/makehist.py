98395e9550c231034cfe95d0c7ba2f4b23e57521
micahc <micahc@b3ba8586-d01d-0410-af51-a98ec15c9dce>
2010-07-05 10:45:18 +0000

    tiny fix to makehist

--- scripts/makehist.py
+++ scripts/makehist.py
@@ -198,7 +198,6 @@ if __name__ == "__main__":
         except:
             print "No ground truth for bold available, if you have some put it in ", \
                         sys.argv[1] + "truebold.nii.gz"
-        P.plot([val[0] for val in stims], [.005*val[1] - .01 for val in stims], 'r')
         print "params"
         print getparams_mu(-1, histimg, pos)
         print getA(getparams_mu(-1, histimg, pos)[2])
